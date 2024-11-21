5f0b80a2034723c8e9ad6e084944524b71c8eaca
Alain Ramon <alan_ramon94@hotmail.com>
2024-11-21 09:34:11 +0100

    ramon push

--- server/py/hangman.py
+++ server/py/hangman.py
@@ -31,7 +31,7 @@ class Hangman(Game):
     def __init__(self) -> None:
         """ Important: Game initialization also requires a set_state call to set the 'word_to_guess' """
         self.state = None
-        initial_state = HangmanGameState(word_to_guess=word_to_guess,
+        initial_state = HangmanGameState(word_to_guess="DevOps",
                                          phase=GamePhase.SETUP,
                                          guesses=[],
                                          incorrect_guesses=[])
