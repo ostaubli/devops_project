6b513d80509d360345d3abe9c7cbef9e3f0449ea
oliverheisel <oliver@heisel.one>
2024-11-24 16:41:07 +0100

    debugging - game is working now

--- server/py/hangman.py
+++ server/py/hangman.py
@@ -14,7 +14,6 @@ class GamePhase(str, Enum):
     SETUP = 'setup'            # before the game has started
     RUNNING = 'running'        # while the game is running
     FINISHED = 'finished'      # when the game is finished
-    #add comment
 
 
 class HangmanGameState:
