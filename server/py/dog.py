abefd5df28b1dd2a9c87f69c0b9fa8f9ab7dc6fd
Daniela Reichmuth <daniela.reichmuth@stud.hslu.ch>
2024-12-09 13:15:43 +0100

    apply action for ace

--- server/py/dog.py
+++ server/py/dog.py
@@ -640,6 +640,22 @@ class Dog(Game):
                         print(f"{player.name}'s marble moved 13 steps forward to position {action.pos_to}.")
                         break
 
+        elif action.card.rank == 'A':
+            if action.pos_from in self.board["kennel_positions"][player.index]:  # Leaving the kennel
+                for marble in player.list_marble:
+                    if marble.pos == action.pos_from:
+                        marble.pos = action.pos_to
+                        marble.is_save = True
+                        print(f"{player.name}'s marble moved out of the kennel to position {action.pos_to}.")
+                        break
+            else:  # Moving 1 or 11 steps forward
+                for marble in player.list_marble:
+                    if marble.pos == action.pos_from and marble.is_save:
+                        marble.pos = action.pos_to
+                        steps = action.pos_to - action.pos_from
+                        print(f"{player.name}'s marble moved {steps} steps forward to position {action.pos_to}.")
+                        break
+
         elif action.card.rank == 'JKR':  # Joker: use as any card
             # Action can be anything based on the game rules, e.g., swap a card or move a marble
             pass
