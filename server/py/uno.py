8f9b96a2583ec99e22f75f1b6f23a3b98cead323
Albin Plathottathil <albin.plathottathil@gmail.com>
2024-12-09 15:42:45 +0100

    merge changes from oliver's branch

--- server/py/uno.py
+++ server/py/uno.py
@@ -1,6 +1,3 @@
-# -runcmd: cd ../.. & venv\Scripts\python server/py/uno.py
-# runcmd: cd ../.. & venv\Scripts\python benchmark/benchmark_uno.py python uno.Uno
-
 from server.py.game import Game, Player
 from typing import List, Optional
 from pydantic import BaseModel
@@ -49,6 +46,53 @@ class GameState(BaseModel):
     CNT_HAND_CARDS: int = 7
     LIST_COLOR: List[str] = ['red', 'green', 'yellow', 'blue', 'any']
     LIST_SYMBOL: List[str] = ['skip', 'reverse', 'draw2', 'wild', 'wilddraw4']
+    LIST_CARD: List[Card] = [
+        Card(color='red', number=0), Card(color='green', number=0), Card(color='yellow', number=0), Card(color='blue', number=0),
+        Card(color='red', number=1), Card(color='green', number=1), Card(color='yellow', number=1), Card(color='blue', number=1),
+        Card(color='red', number=2), Card(color='green', number=2), Card(color='yellow', number=2), Card(color='blue', number=2),
+        Card(color='red', number=3), Card(color='green', number=3), Card(color='yellow', number=3), Card(color='blue', number=3),
+        Card(color='red', number=4), Card(color='green', number=4), Card(color='yellow', number=4), Card(color='blue', number=4),
+        Card(color='red', number=5), Card(color='green', number=5), Card(color='yellow', number=5), Card(color='blue', number=5),
+        Card(color='red', number=6), Card(color='green', number=6), Card(color='yellow', number=6), Card(color='blue', number=6),
+        Card(color='red', number=7), Card(color='green', number=7), Card(color='yellow', number=7), Card(color='blue', number=7),
+        Card(color='red', number=8), Card(color='green', number=8), Card(color='yellow', number=8), Card(color='blue', number=8),
+        Card(color='red', number=9), Card(color='green', number=9), Card(color='yellow', number=9), Card(color='blue', number=9),
+        Card(color='red', number=1), Card(color='green', number=1), Card(color='yellow', number=1), Card(color='blue', number=1),
+        Card(color='red', number=2), Card(color='green', number=2), Card(color='yellow', number=2), Card(color='blue', number=2),
+        Card(color='red', number=3), Card(color='green', number=3), Card(color='yellow', number=3), Card(color='blue', number=3),
+        Card(color='red', number=4), Card(color='green', number=4), Card(color='yellow', number=4), Card(color='blue', number=4),
+        Card(color='red', number=5), Card(color='green', number=5), Card(color='yellow', number=5), Card(color='blue', number=5),
+        Card(color='red', number=6), Card(color='green', number=6), Card(color='yellow', number=6), Card(color='blue', number=6),
+        Card(color='red', number=7), Card(color='green', number=7), Card(color='yellow', number=7), Card(color='blue', number=7),
+        Card(color='red', number=8), Card(color='green', number=8), Card(color='yellow', number=8), Card(color='blue', number=8),
+        Card(color='red', number=9), Card(color='green', number=9), Card(color='yellow', number=9), Card(color='blue', number=9),
+        # skip next player
+        Card(color='red', symbol='skip'), Card(color='green', symbol='skip'), Card(color='yellow', symbol='skip'), Card(color='blue', symbol='skip'),
+        Card(color='red', symbol='skip'), Card(color='green', symbol='skip'), Card(color='yellow', symbol='skip'), Card(color='blue', symbol='skip'),
+        # revers playing direction
+        Card(color='red', symbol='reverse'), Card(color='green', symbol='reverse'), Card(color='yellow', symbol='reverse'), Card(color='blue', symbol='reverse'),
+        Card(color='red', symbol='reverse'), Card(color='green', symbol='reverse'), Card(color='yellow', symbol='reverse'), Card(color='blue', symbol='reverse'),
+        # next player must draw 2 cards
+        Card(color='red', symbol='draw2'), Card(color='green', symbol='draw2'), Card(color='yellow', symbol='draw2'), Card(color='blue', symbol='draw2'),
+        Card(color='red', symbol='draw2'), Card(color='green', symbol='draw2'), Card(color='yellow', symbol='draw2'), Card(color='blue', symbol='draw2'),
+        # current player choses color for next player to play
+        Card(color='any', symbol='wild'), Card(color='any', symbol='wild'),
+        Card(color='any', symbol='wild'), Card(color='any', symbol='wild'),
+        # current player choses color for next player to play and next player must draw 4 cards
+        Card(color='any', symbol='wilddraw4'), Card(color='any', symbol='wilddraw4'),
+        Card(color='any', symbol='wilddraw4'), Card(color='any', symbol='wilddraw4'),
+    ]
+
+    # list_card_draw: Optional[List[Card]]     # list of cards to draw
+    # list_card_discard: Optional[List[Card]]  # list of cards discarded
+    # list_player: List[PlayerState]           # list of player-states
+    # phase: GamePhase                         # the current game-phase ("setup"|"running"|"finished")
+    # cnt_player: int                          # number of players N (to be set in the phase "setup")
+    # idx_player_active: Optional[int]         # the index (0 to N-1) of active player
+    # direction: int                           # direction of the game, +1 to the left, -1 to right
+    # color: str                               # active color (last card played or the chosen color after a wild cards)
+    # cnt_to_draw: int                         # accumulated number of cards to draw for the next player
+    # has_drawn: bool                          # flag to indicate if the last player has alreay drawn cards or not
 
     # Dynamic state
     list_card_draw: List[Card] = []
