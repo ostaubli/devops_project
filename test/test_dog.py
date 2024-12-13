7dfe0a24a339befba602be9d64bf4a1e43071eea
Andri Gerber <andri.gerber@stud.hslu.ch>
2024-12-13 15:20:02 +0100

    added tests for all da_53-57 of [Project 47]

--- test/test_dog.py
+++ test/test_dog.py
@@ -50,6 +50,11 @@ def test_action():
     assert "card=" in repr(a)
 
 
+# -------------------------------------------------------
+# GamePhase und GameState Tests
+# -------------------------------------------------------
+
+
 def test_game_phase():
     assert GamePhase.SETUP == "setup"
     assert GamePhase.RUNNING == "running"
@@ -71,6 +76,168 @@ def test_game_state():
     )
     assert state.cnt_player == 4
     assert state.phase == GamePhase.SETUP
+
+
+# -------------------------------------------------------
+# Dog-Game Grundfunktionalitäten
+# -------------------------------------------------------
+
+
+def test_dog_init(game):
+    state = game.get_state()
+    assert state.phase == GamePhase.RUNNING
+    assert len(state.list_player) == 4
+
+
+def test_dog_reset(game):
+    game.state.cnt_round = 5
+    game.reset()
+    assert game.state.cnt_round == 1
+
+
+def test_dog_set_state(game):
+    new_state = copy.deepcopy(game.get_state())
+    new_state.cnt_round = 3
+    game.set_state(new_state)
+    assert game.get_state().cnt_round == 3
+
+
+def test_dog_print_state(game):
+    # print_state ist leer, wir testen nur, dass kein Fehler auftritt
+    game.print_state()
+
+
+def test_setup_next_round(game):
+    # Runde künstlich verändern
+    game.state.cnt_round = 2
+    old_len = len(game.state.list_player[0].list_card)
+    game.setup_next_round()
+    assert len(game.state.list_player[0].list_card) >= old_len
+
+
+def test_next_turn(game):
+    old_active = game.state.idx_player_active
+    game.next_turn()
+    assert game.state.idx_player_active != old_active
+
+
+def test_player_finished(game):
+    # Niemand sollte am Anfang fertig sein.
+    assert not game._player_finished(0)
+
+
+def test_controlled_player_indices(game):
+    indices = game._controlled_player_indices()
+    assert isinstance(indices, list)
+
+
+def test_get_player_marbles(game):
+    marbles = game._get_player_marbles()
+    assert isinstance(marbles, list)
+    assert len(marbles) > 0
+
+
+# -------------------------------------------------------
+# Kartenaktionen und Hilfsfunktionen
+# -------------------------------------------------------
+
+
+def test_get_start_actions(game):
+    c = Card(suit='♥', rank='A')
+    actions = game._get_start_actions(c)
+    assert isinstance(actions, list)
+
+
+def test_get_safe_marble_actions_for_jack(game):
+    actions = game._get_safe_marble_actions_for_jack([], Card(suit='♥', rank='J'))
+    assert actions == []
+
+
+def test_get_jack_actions(game):
+    c = Card(suit='♥', rank='J')
+    actions = game._get_jack_actions(c)
+    assert isinstance(actions, list)
+
+
+def test_get_standard_actions(game):
+    c = Card(suit='♥', rank='5')  # 5 = 5 Felder
+    actions = game._get_standard_actions(c, 5)
+    assert isinstance(actions, list)
+
+
+def test_calc_pos_to(game):
+    pos_to = game._calc_pos_to(pos_from=0, dist=5, player_idx=0, rank='5')
+    assert pos_to is not None
+
+
+def test_count_steps_to_finish(game):
+    steps = game._count_steps_to_finish(0, game.PLAYER_BOARD_SEGMENTS[0]['start'], game.PLAYER_BOARD_SEGMENTS[0]['final_start'])
+    assert isinstance(steps, int)
+
+
+def test_get_actions_for_seven_card(game):
+    actions = game._get_actions_for_seven_card()
+    assert actions == []
+
+
+def test_get_actions_for_joker(game):
+    c = Card(suit='♥', rank='JKR')
+    actions = game._get_actions_for_joker(c, [])
+    assert isinstance(actions, list)
+
+
+def test_get_actions_for_card(game):
+    c = Card(suit='♥', rank='A')
+    actions = game._get_actions_for_card(c)
+    assert isinstance(actions, list)
+
+
+def test_get_list_action(game):
+    actions = game.get_list_action()
+    assert isinstance(actions, list)
+
+
+def test_unique_sorted_actions(game):
+    a1 = Action(card=Card(suit='♥', rank='A'), pos_from=10, pos_to=20)
+    a2 = Action(card=Card(suit='♥', rank='A'), pos_from=10, pos_to=30)
+    sorted_actions = game._unique_sorted_actions([a1, a2, a1])
+    assert len(sorted_actions) == 2
+
+
+def test_is_valid_move(game):
+    valid = game.is_valid_move(64, 0)
+    assert isinstance(valid, bool)
+
+
+def test_can_start(game):
+    can = game._can_start(game.PLAYER_BOARD_SEGMENTS[0]['start'])
+    assert isinstance(can, bool)
+
+
+def test_blocked_on_main_path(game):
+    blocked = game._blocked_on_main_path(0)
+    assert isinstance(blocked, bool)
+
+
+def test_move_through_main_path(game):
+    can_move = game._move_through_main_path(0, 5, 1)
+    assert isinstance(can_move, bool)
+
+
+def test_move_through_final_area(game):
+    fs = game.PLAYER_BOARD_SEGMENTS[0]['final_start']
+    can_move = game._move_through_final_area(fs, 2)
+    assert isinstance(can_move, bool)
+
+
+def test_path_clear(game):
+    clear = game._path_clear(0, 10, 0)
+    assert isinstance(clear, bool)
+
+
+# -------------------------------------------------------
+# Aktionen ausführen und Spielstatus
+# -------------------------------------------------------
 
 
 def test_find_marble_by_pos(game):
@@ -99,88 +266,61 @@ def test_handle_jack_action(game):
     action = Action(pos_from=64, pos_to=65)
     game._handle_jack_action(action)
 
-# TODO: These are the old tests, they need to be updated or removed. Insert new tests above!
-# def test_initial_game_state_values(game):
-#     """Test 001: Validate values of initial game state (cnt_round=1) [5 points]"""
-#     state = game.get_state()
-#
-#     assert state.phase == GamePhase.RUNNING, f'{state}Error: "phase" must be GamePhase.RUNNING initially'
-#     assert state.cnt_round == 1, f'{state}Error: "cnt_round" must be 1 initially'
-#     assert len(state.list_card_discard) == 0, f'{state}Error: len("list_card_discard") must be 0 initially'
-#     assert len(state.list_card_draw) == 86, f'{state}Error: len("list_card_draw") must be 86 initially'
-#     assert len(state.list_player) == 4, f'{state}Error: len("list_player") must be 4'
-#     assert state.idx_player_active >= 0, f'{state}Error: "idx_player_active" must be >= 0'
-#     assert state.idx_player_active < 4, f'{state}Error: "idx_player_active" must be < 4'
-#     assert state.idx_player_started == state.idx_player_active, f'{state}Error: "idx_player_active" must be == "idx_player_started"'
-#     assert state.card_active is None, f'{state}Error: "card_active" must be None'
-#     assert not state.bool_card_exchanged, f'{state}Error: "bool_card_exchanged" must be False'
-#
-#     for player in state.list_player:
-#         assert len(player.list_card) == 6, f'{state}Error: len("list_player.list_card") must be 6 initially'
-#         assert len(player.list_marble) == 4, f'{state}Error: len("list_player.list_marble") must be 4 initially'
-#
-#
-# def test_starting_player_is_active(game):
-#     """Test 002: Verify that the correct player starts the game [5 points]"""
-#     state = game.get_state()
-#     assert 0 <= state.idx_player_started < 4, f'{state}Error: "idx_player_started" must be in range (0-3)'
-#     assert state.idx_player_active == state.idx_player_started, f'{state}Error: "idx_player_active" must be equal to "idx_player_started" initially'
-#
-#
-# def test_set_custom_game_state():
-#     """Test 003: Set and validate a custom game state [5 points]"""
-#     game = Dog()
-#     new_state = GameState(
-#         cnt_player=4,
-#         phase=GamePhase.RUNNING,
-#         cnt_round=2,
-#         bool_card_exchanged=True,
-#         idx_player_started=1,
-#         idx_player_active=2,
-#         list_player=[
-#             PlayerState(name="Player 1", list_card=[], list_marble=[Marble(pos=None, is_save=False) for _ in range(4)]),
-#             PlayerState(name="Player 2", list_card=[], list_marble=[Marble(pos=None, is_save=False) for _ in range(4)]),
-#             PlayerState(name="Player 3", list_card=[], list_marble=[Marble(pos=None, is_save=False) for _ in range(4)]),
-#             PlayerState(name="Player 4", list_card=[], list_marble=[Marble(pos=None, is_save=False) for _ in range(4)])
-#         ],
-#         list_card_draw=[],
-#         list_card_discard=[],
-#         card_active=None
-#     )
-#
-#     game.set_state(new_state)
-#     state = game.get_state()
-#
-#     assert state.cnt_round == 2, f'{state}Error: "cnt_round" must be 2'
-#     assert state.idx_player_active == 2, f'{state}Error: "idx_player_active" must be 2'
-#
-#
-# def test_initial_marble_positions(game):
-#     """Test 004: Validate initial positions of all marbles [5 points]"""
-#     state = game.get_state()
-#     for player in state.list_player:
-#         for marble in player.list_marble:
-#             assert marble.pos is None, f'{state}Error: "marble.pos" must be None initially'
-#             assert not marble.is_save, f'{state}Error: "marble.is_save" must be False initially'
-#
-#
-# def test_initial_card_draw_pile(game):
-#     """Test 005: Validate the number of cards in the draw pile after initial deal [5 points]"""
-#     state = game.get_state()
-#     assert len(state.list_card_draw) == 86, f'{state}Error: "list_card_draw" must contain 86 cards initially'
-#
-#
-# def test_number_of_cards_in_round_2():
-#     """Test 047: Validate number of cards dealt in Round 2 [5 points]"""
-#     # Initialize the game
-#     game = Dog()
-#     # Manually set up the game to round 2
-#     game.next_turn()  # Simulate turns until a new round starts
-#     # Set up next round (should be round 2)
-#     while game.get_state().cnt_round == 1: # reapeate for 3, 4, 5, 6
-#         game.next_turn()
-#     # Now we're in round 2, verify that each player has 5 cards
-#     state = game.get_state()
-#     for player in state.list_player:
-#         # reapeate for 3 -> 4, 4 -> 3, 5 -> 2, 6-> 6
-#         assert len(player.list_card) == 5, f'Error: Expected 5 cards in round 2, found {len(player.list_card)}'
+
+def test_apply_action_none(game):
+    old_active = game.state.idx_player_active
+    game.apply_action(None)
+    assert game.state.idx_player_active != old_active
+
+
+def test_calc_steps(game):
+    steps = game._calc_steps(64, game.PLAYER_BOARD_SEGMENTS[0]['start'], 0)
+    assert steps is not None
+
+
+def test_check_game_status(game):
+    game.check_game_status()
+    assert game.state.phase == GamePhase.RUNNING
+
+
+def test_get_move_distance(game):
+    c = Card(suit='♥', rank='A')
+    dist = game.get_move_distance(c)
+    assert dist == [1, 11]
+
+
+def test_get_player_view(game):
+    view = game.get_player_view(0)
+    assert isinstance(view, GameState)
+
+
+def test_swap_cards_final(game):
+    p1 = game.state.list_player[0]
+    p2 = game.state.list_player[2]
+    c1 = p1.list_card[0]
+    c2 = p2.list_card[0]
+    game.swap_cards(0, 2, c1, c2)
+    assert c1 in p2.list_card
+    assert c2 in p1.list_card
+
+
+def test_random_player():
+    rp = RandomPlayer()
+    state = GameState(
+        cnt_player=4,
+        phase=GamePhase.RUNNING,
+        cnt_round=1,
+        bool_card_exchanged=False,
+        idx_player_started=0,
+        idx_player_active=0,
+        list_player=[],
+        list_card_draw=[],
+        list_card_discard=[],
+        card_active=None
+    )
+    actions = [Action(card=Card(suit='♥', rank='A'), pos_from=64, pos_to=0)]
+    chosen = rp.select_action(state, actions)
+    assert chosen in actions or chosen is None
+    rp.do_nothing()
+
+
